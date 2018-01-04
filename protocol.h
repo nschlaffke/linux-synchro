1590aa4b1e05ccbeaca0787b5802743e7b3fc397
Norbert Schlaffke <norbert.schlaffke@student.cs.put.poznan.pl>
2018-01-04 13:56:02 +0100

    [F] Create basic server draft and protocol file

--- protocol.h
+++ protocol.h
@@ -5,4 +5,10 @@
 #ifndef DROPBOX_PROTOCOL_H
 #define DROPBOX_PROTOCOL_H
 
+#define HEARTBEAT -1
+#define NEW_CLIENT 0
+#define NEW_FILE 1
+#define DELETE_FILE 2
+#define MOVE_FILE 3
+
 #endif //DROPBOX_PROTOCOL_H
--- server.cpp
+++ server.cpp
@@ -1,4 +1,83 @@
 //
 // Created by ns on 04.01.18.
 //
+#include <iostream>
+#include "TcpServer.h"
+#include "protocol.h"
 
+#define DEBUG
+
+#include <string>
+#include <vector>
+#include <mutex>
+#include <thread>
+
+
+using namespace std;
+void acceptClients(TcpServer tcpServer, mutex clientsMutex, vector<TcpSocket> clients);
+
+int main(int argc, char *argv[])
+{
+#ifndef DEBUG
+    if(argc != 3)
+    {
+        cout << "enter: server.cpp [ip] [port]\n";
+        return 0;
+    }
+#else
+    const string ip = "192.168.8.105";
+    const short port = 8888;
+#endif
+    std::mutex clientsMutex;
+    std::vector<TcpSocket> clients;
+    TcpServer tcpServer;
+    std::thread acceptor(&acceptClients, tcpServer, clientsMutex, clients); // nasluchiwanie na nowych klientow
+    const int bufferSize = 1024;
+    const int messageSize = sizeof(char);
+    while ((true))
+    {
+        for(TcpSocket sock : clients)
+        {
+            if(!sock.hasData())
+            {
+                // nic nie wysyla
+                // TODO implement heartbeat protocol
+                continue;
+            }
+            // pobieramy numerek zdarzenia
+            int messageBuffer = 0;
+            int tmp;
+            tmp = sock.recieveData(reinterpret_cast<char *>(&messageBuffer), messageSize);
+            if (tmp < 0)
+            {
+               cout << "ERROR: COULDN'T RECIEVE MESSAGE\n";
+                return 0;
+            }
+            switch(messageBuffer)
+            {
+                case NEW_CLIENT:
+                    break;
+
+                case NEW_FILE:
+                    break;
+
+                case DELETE_FILE:
+                    break;
+
+                case MOVE_FILE:
+                    break;
+            }
+        }
+    }
+}
+
+void acceptClients(TcpServer tcpServer, mutex &clientsMutex, vector<TcpSocket> &clients)
+{
+    while(true)
+    {
+        TcpSocket tmp = tcpServer.doAccept();
+        clientsMutex.lock();
+        clients.push_back(tmp);
+        clientsMutex.unlock();
+    }
+}
