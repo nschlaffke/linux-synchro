43375bcac762f07d9c68ab87faaacf7fb0d206b9
Norbert Schlaffke <norbert.schlaffke@student.cs.put.poznan.pl>
2018-01-15 18:47:42 +0100

    [F] Implement thread-safe queue template: SafeQueue

---
+++ SafeQueue.h
@@ -0,0 +1,46 @@
+//
+// Created by ns on 15.01.18.
+//
+
+#ifndef DROPBOX_SAFEQUEUE_H
+#define DROPBOX_SAFEQUEUE_H
+
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+
+template <class T>
+class SafeQueue
+{
+public:
+    void enqueue(T &element);
+    T dequeue();
+
+private:
+    std::queue<T> q;
+    std::mutex m;
+    std::condition_variable c;
+};
+
+template <class T>
+void SafeQueue<T>::enqueue(T &element)
+{
+    std::lock_guard<std::mutex> lock(m);
+    q.push(element);
+    c.notify_one();
+}
+
+template <class T>
+T SafeQueue<T>::dequeue()
+{
+    std::unique_lock<std::mutex> l(m);
+    while(q.empty())
+    {
+        c.wait(l);
+    }
+    T element = q.front();
+    q.pop();
+    return element;
+}
+
+#endif //DROPBOX_SAFEQUEUE_H
