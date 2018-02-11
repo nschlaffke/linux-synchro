96fca62bca22e621477623403ed8ffe87298f836
norbert.schlaffke <norbert.schlaffke@student.put.poznan.pl>
2018-02-11 15:16:46 +0100

    [F] improve cyclic.sh

--- test/cyclic.sh
+++ test/cyclic.sh
@@ -1,22 +1,28 @@
 #!/usr/bin/env bash
+
 if test "$#" -ne 1; then
     echo "podaj folder klienta 1"
     exit
 fi
 cd $1
+mkdir folder
+cd folder
 pwd
 files=("plik1.txt" "plik2.txt" "plik3.txt")
 while true
 do
+    mkdir f
+    cd f
     for file in ${files[*]}
     do
         ls -al > $file
     done
-    sleep 1
+    sleep 3
     for file in ${files[*]}
     do
-
         rm $file
     done
-    sleep 1
+    sleep 3
+    cd ..
+    rm f
 done
